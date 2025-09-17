4e9d11cfbcaa39f6c74afeab1afa5ccf8efc8afc
tobiaslinner <96toblin@skola.boras.se>
2025-09-17 18:10:59 +0200

    git init

---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import LoginForm from '@/components/login-form';
+import { Image } from 'expo-image';
+import { ScrollView, StyleSheet, View } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <ScrollView contentContainerStyle={styles.root}>
+      <Image
+        source={require('../assets/images/background.png')}
+        style={styles.pluckyImage}
+      />
+      <View>
+        <LoginForm />
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    gap: 8
+  },
+  pluckyImage: {
+    height: 300,
+    width: '100%',
+  },
+});
